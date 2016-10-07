63024f64122f28cddbdd71011726e710624a65f4
harmoh <harmoh@me.com>
2016-10-07 12:34:10 +0200

    Added vec3 class

---
+++ vec3.cpp
@@ -0,0 +1,91 @@
+#include "vec3.h"
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+vec3::vec3()
+{
+
+}
+vec3::vec3(double x, double y, double z)
+{
+    components[0] = x;
+    components[1] = y;
+    components[2] = z;
+}
+
+double vec3::lengthSquared()
+{
+    // Returns the square of the length (or norm) of the vector
+    return components[0]*components[0]+components[1]*components[1]+components[2]*components[2];
+}
+
+double vec3::length()
+{
+    // Returns the length (or norm) of the vector
+    return sqrt(lengthSquared());
+}
+
+vec3 &vec3::operator+=(double rhs)
+{
+    components[0] += rhs;
+    components[1] += rhs;
+    components[2] += rhs;
+    return *this;
+}
+
+vec3 &vec3::operator+=(vec3 rhs)
+{
+    components[0] += rhs[0];
+    components[1] += rhs[1];
+    components[2] += rhs[2];
+    return *this;
+}
+
+vec3 &vec3::operator*=(double rhs)
+{
+    components[0] *= rhs;
+    components[1] *= rhs;
+    components[2] *= rhs;
+    return *this;
+}
+
+vec3 &vec3::operator*=(vec3 rhs)
+{
+    components[0] *= rhs[0];
+    components[1] *= rhs[1];
+    components[2] *= rhs[2];
+    return *this;
+}
+
+vec3 &vec3::operator-=(double rhs)
+{
+    components[0] -= rhs;
+    components[1] -= rhs;
+    components[2] -= rhs;
+    return *this;
+}
+
+vec3 &vec3::operator-=(vec3 rhs)
+{
+    components[0] -= rhs[0];
+    components[1] -= rhs[1];
+    components[2] -= rhs[2];
+    return *this;
+}
+
+vec3 &vec3::operator/=(double rhs)
+{
+    components[0] /= rhs;
+    components[1] /= rhs;
+    components[2] /= rhs;
+    return *this;
+}
+
+vec3 &vec3::operator/=(vec3 rhs)
+{
+    components[0] /= rhs[0];
+    components[1] /= rhs[1];
+    components[2] /= rhs[2];
+    return *this;
+}
---
+++ vec3.h
@@ -0,0 +1,92 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+
+class vec3
+{
+public:
+    vec3();
+    vec3(double x, double y, double z);
+    double lengthSquared();
+    double length();
+
+    // Actions
+
+    // Operators
+    double &operator()(int index) { return components[index]; } // Allows access like myVector(0)
+    double &operator[](int index) { return components[index]; } // Allows access like myVector[0]
+    vec3 &operator+=(double rhs); // Componentwise addition with scalar
+    vec3 &operator+=(vec3 rhs);   // Componentwise addition with vector
+    vec3 &operator*=(double rhs); // Componentwise multiplication with scalar
+    vec3 &operator*=(vec3 rhs);   // Componentwise multiplicationwith vector
+    vec3 &operator-=(double rhs); // Componentwise subtraction with scalar
+    vec3 &operator-=(vec3 rhs);   // Componentwise subtraction with vector
+    vec3 &operator/=(double rhs); // Componentwise division with scalar
+    vec3 &operator/=(vec3 rhs);   // Componentwise division with vector
+private:
+    double components[3];
+};
+
+inline vec3 operator+(vec3 lhs, double rhs) {
+    lhs += rhs;
+    return lhs;
+}
+
+inline vec3 operator+(double lhs, vec3 rhs) {
+    rhs += lhs;
+    return rhs;
+}
+
+inline vec3 operator+(vec3 lhs, vec3 rhs) {
+    lhs += rhs;
+    return lhs;
+}
+
+inline vec3 operator-(vec3 lhs, double rhs) {
+    lhs -= rhs;
+    return lhs;
+}
+
+inline vec3 operator-(double lhs, vec3 rhs) {
+    rhs -= lhs;
+    return rhs;
+}
+
+inline vec3 operator-(vec3 lhs, vec3 rhs) {
+    lhs -= rhs;
+    return lhs;
+}
+
+
+inline vec3 operator*(vec3 lhs, double rhs) {
+    lhs *= rhs;
+    return lhs;
+}
+
+inline vec3 operator*(double lhs, vec3 rhs) {
+    rhs *= lhs;
+    return rhs;
+}
+
+inline vec3 operator*(vec3 lhs, vec3 rhs) {
+    lhs *= rhs;
+    return lhs;
+}
+
+
+inline vec3 operator/(vec3 lhs, double rhs) {
+    lhs /= rhs;
+    return lhs;
+}
+
+inline vec3 operator/(double lhs, vec3 rhs) {
+    rhs /= lhs;
+    return rhs;
+}
+
+inline vec3 operator/(vec3 lhs, vec3 rhs) {
+    lhs /= rhs;
+    return lhs;
+}
+
+#endif // VEC3_H
