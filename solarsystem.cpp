36499299f6211da884f64be31293dd2d1e4ebc3a
harmoh <harmoh@me.com>
2016-10-24 20:39:04 +0200

    Final changes

--- solarsystem.cpp
+++ solarsystem.cpp
@@ -86,7 +86,7 @@ void SolarSystem::calculateForcesAndEnergy()
 
             m_angularMomentum += deltaRVector.cross(body1.velocity);
         }
-        m_potentialEnergy += body1.force.length() * body1.position.length();
+        m_potentialEnergy -= body1.force.length() * body1.position.length();
         m_kineticEnergy += 0.5 * body1.mass * body1.velocity.lengthSquared();
     }
 }
